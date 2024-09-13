54bad66cd317be1a0d31ae70e994d407d2bda874
BrayanRuiz77 <andresbrayanpe@gmail.com>
2024-09-13 14:34:18 -0500

    firs comen

--- lib/main.dart
+++ lib/main.dart
@@ -25,7 +25,7 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
-HoldScrollActivity
+
   final String title;
 
   @override
